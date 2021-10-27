b7e6893ec3d0126fe308d280c8cf9411dcc4b000
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-10-27 17:44:00 +0200

    Create prodEscalar.java

---
+++ PCTR/RELACION3/EJ1/prodEscalar.java
@@ -0,0 +1,23 @@
+import java.lang.Math;
+
+/**
+ * La clase prodEscalar multiplica dos vectores de tipo Double y lo guarda en el
+ * primer vector.
+ * 
+ * @author Javier López Sierra
+ */
+public class prodEscalar {
+    public static void main(String[] args) {
+        double[] vec1 = new double[(int) Math.pow(10, 6)]; //declaramos e inicializamos vector 1
+        for (int i = 0; i < vec1.length; i++) {
+            vec1[i] = Math.random();
+        }
+        double[] vec2 = new double[(int) Math.pow(10, 6)]; //declaramos e inicializamos vector 2
+        for (int i = 0; i < vec2.length; i++) {
+            vec2[i] = Math.random();
+        }
+        for (int i = 0; i < vec1.length; i++) { //hacemos el producto
+            vec1[i] = vec1[i] * vec2[i];
+        }
+    }
+}
