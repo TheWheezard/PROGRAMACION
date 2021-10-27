96ec4173d34a5ea7602fa8a8a866d84008a5cec2
Javi Lopez <javi.lopezsierra@alum.uca.es>
2021-10-27 20:10:31 +0200

    Delete PCTR/RELACION2/ALICIA directory

--- PCTR/RELACION2/ALICIA/Critica.java
+++
@@ -1,9 +0,0 @@
-public class Critica
-{
-    private int n=0;
-    
-    public Critica() {}
-    public void incremento(){n++;}
-    public void decremento(){n--;}
-    public int vDato(){return(n);}
-}
