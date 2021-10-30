655ce22938bf98171db01a4224dab6ea2dd0e04d
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-10-30 14:49:26 +0200

    P3E1-fin

---
+++ PCTR/RELACION3/EJ1/ClasePrincipalParalelo.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+/**
+ * La clase ClasePrincipalParalelo crea dos vectores de tipo double de misma
+ * longitud y los divide en partes que se multiplican de forma concurrente
+ * mediante el uso de la clase prodEscalarParalelo.
+ * 
+ * @author Javier López Sierra
+ * @see prodEscalarParalelo
+ */
+public class ClasePrincipalParalelo {
+    static double[] vec1 = new double[(int) Math.pow(10, 6)];
+    static double[] vec2 = new double[(int) Math.pow(10, 6)];
+
+    public static void main(String[] args) {
+        for (int i = 0; i < vec1.length; i++) { // rellenamos vec1 y vec2
+            vec1[i] = vec2[i] = Math.random();
+        }
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduzca número de partes en que partir los vectores:");
+        int partes = scan.nextInt();
+        prodEscalarParalelo[] productos = new prodEscalarParalelo[partes];
+        Thread[] hilos = new Thread[partes];
+        int j = 0;
+        for (int i = 0; i < partes; i++) { // creamos los hilos con los rangos que van a usar
+            hilos[i] = productos[i] = new prodEscalarParalelo(i, j, vec1.length * (i + 1) / partes);
+            j = vec1.length * (i + 1) / partes;
+        }
+        // inicio y fin de la ejecución de los hilos
+        for (int i = 0; i < hilos.length; i++) {
+            hilos[i].start();
+        }
+        for (int i = 0; i < hilos.length; i++) {
+            try {
+                hilos[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        // recuperando resultados de los hilos en el vector productoParcial
+        double[] productoParcial = new double[(int) Math.pow(10, 6)];
+        int iteradorParcial = 0;
+        int i = 0;
+        // primer bucle: se ejecuta mientras no lleguemos al final del array productos o de productoParcial
+        while (i < productos.length && iteradorParcial < productoParcial.length) {
+            System.out.println("Recuperando resultados del hilo " + productos[i].getIdHebra());
+            int k = 0;
+            // segundo bucle: se ejecuta mientras no lleguemos al final del array interno de productos[i] o de productoParcial
+            while (k < productos[i].getTam() && iteradorParcial < productoParcial.length) {
+                productoParcial[iteradorParcial] = productos[i].getRes(k);
+                k++;
+                iteradorParcial++;
+            }
+            i++;
+        }
+        scan.close();
+    }
+}
--- PCTR/RELACION3/EJ1/prodEscalarParalelo.java
+++ PCTR/RELACION3/EJ1/prodEscalarParalelo.java
@@ -1,13 +1,89 @@
-/** */
+/**
+ * La clase prodEscalarParalelo crea un hilo con un vector auxiliar donde efectuará el
+ * producto escalar entre los elementos comprendidos entre dos posiciones dadas
+ * de dos vectores principales.
+ * 
+ * @author Javier López Sierra
+ * @see Java.lang.Thread
+ */
 public class prodEscalarParalelo extends Thread {
-    static double[] vec1 = new double[(int) Math.pow(10, 6)];
-    static double[] vec2 = new double[(int) Math.pow(10, 6)];
+    //static double[] vec1 = new double[(int) Math.pow(10, 6)];
+    //static double[] vec2 = new double[(int) Math.pow(10, 6)];
+    static double[] vecAux;
     int idHebra, inicio, fin;
-    
+
+    /**
+     * Constructor de la clase prodEscalarParalelo.
+     * 
+     * @param idHebra Parámetro de tipo Int que identificará el número de
+     *                hebra/vector.
+     * @param inicio  Parámetro de tipo Int que indicará la posición inicial del
+     *                rango de posiciones en los vectores principales donde se hará
+     *                el cálculo.
+     * @param fin     Parámetro de tipo Int que indicará la posición final del rango
+     *                de posiciones en los vectores principales donde se hará el
+     *                cálculo.
+     */
     public prodEscalarParalelo(int idHebra, int inicio, int fin) {
         this.idHebra = idHebra;
         this.inicio = inicio;
         this.fin = fin;
+        vecAux = new double[fin - inicio];
+    }
+
+    /**
+     * Método observador que devuelve el identificador de la hebra/vector
+     * resultante.
+     * 
+     * @return Valor de tipo Int que identifica la hebre/vector resultante.
+     */
+    public int getIdHebra() {
+        return idHebra;
+    }
+
+    /**
+     * Método que devuelve el tamaño del vector que contiene el resultado.
+     * 
+     * @return valor del tamaño del vector resultado.
+     */
+    public int getTam() {
+        return vecAux.length;
+    }
+
+    /**
+     * Método observador que devuelve el valor de una posición del vector resultante
+     * del producto escalar sobre el rango indicado anteriormente.
+     * 
+     * @param i Posición del vector resultante de la que queremos su valor.
+     * @return Valor de tipo double de la posición i del vector resultante del
+     *         producto escalar.
+     */
+    public double getRes(int i) {
+        return vecAux[i];
+    }
+
+    /**
+     * Método que toma un rango del vector comprendido entre las posiciones inicio y
+     * fin de dos vectores de la clase ClasePrincipalParalelo en la forma [inicio,fin) y los multiplica entre sí,
+     * guardando el resultado en un vector auxiliar.
+     * @see ClasePrincipalParalelo
+     */
+    public void calcular() {
+        int j = 0;
+        for (int i = inicio; i < fin; i++) {
+            vecAux[j] = ClasePrincipalParalelo.vec1[i] * ClasePrincipalParalelo.vec2[i];
+            j++;
+        }
+    }
+
+    /**
+     * Sobrecarga del método run() de la clase Thread que llama al método calcular
+     * de la clase prodEscalarParalelo.
+     * 
+     * @see Java.lang.Thread.run
+     */
+    public void run() {
+        calcular();
     }
 }
 /* continuar desde escalaVPar y VectorPrueba */
