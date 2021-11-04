131e00b57e64088da21d8cb8634edc501cf2e06e
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-11-04 12:29:38 +0100

    Cronómetros
    Agregados cronómetros para controlar el tiempo del cálculo y del proceso completo.

--- PCTR/RELACION3/EJ2/ClasePrincipalConcurrente.java
+++ PCTR/RELACION3/EJ2/ClasePrincipalConcurrente.java
@@ -1,3 +1,5 @@
+import java.text.*;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,7 +10,8 @@ import java.util.Scanner;
  * de forma concurrente.
  * 
  * Está probado con un tamaño máximo de n = 30000 añadiendo necesariamente el
- * flag -Xmx4G. Por sí solo recomendamos no superar n = 10000.
+ * flag -Xmx4G y con un tamaño máximo de n = 50000 con el flag -Xmx10G. Por sí
+ * solo recomendamos no superar n = 10000.
  * 
  * @author Javier López Sierra
  * @see matVectorConcurrente
@@ -22,9 +25,17 @@ public class ClasePrincipalConcurrente {
         Scanner scan = new Scanner(System.in);
         System.out.println("Introduzca tamaño de las matrices:");
         int n = scan.nextInt();
+        System.out.println("Introduzca en cuántas partes se dividirá el proceso:");
+        int numHilos = scan.nextInt();
         matrizCuadrada = new int[n][n];
         matrizB = new int[n];
         int[] matRes = new int[n]; // matriz resultado
+
+        // cronómetro 1 (proceso completo)
+        Date d1 = new Date();
+        DateFormat df1 = new SimpleDateFormat("HH:mm:ss:SSS");
+        long inicCronom1 = System.currentTimeMillis(); // se prepara el cronometro
+        d1.setTime(inicCronom1); // se activa el cronometro
 
         // rellenamos las matrices con valores aleatorios menos res, que será
         // inicializada a 0
@@ -36,8 +47,6 @@ public class ClasePrincipalConcurrente {
             matRes[i] = 0;
         }
 
-        System.out.println("Introduzca en cuántas partes se dividirá el proceso:");
-        int numHilos = scan.nextInt();
         matVectorConcurrente[] matrices = new matVectorConcurrente[numHilos];
         Runnable[] tareas = new Runnable[numHilos];
         Thread[] hilos = new Thread[numHilos];
@@ -46,6 +55,12 @@ public class ClasePrincipalConcurrente {
             tareas[i] = matrices[i] = new matVectorConcurrente(i, n * i / numHilos, j);
             j = n * i / numHilos;
         }
+
+        // cronómetro 2 (cálculo)
+        Date d2 = new Date();
+        DateFormat df2 = new SimpleDateFormat("HH:mm:ss:SSS");
+        long inicCronom2 = System.currentTimeMillis(); // se prepara el cronometro
+        d2.setTime(inicCronom2); // se activa el cronometro
 
         for (int i = 0; i < hilos.length; i++) {
             hilos[i] = new Thread(tareas[i]);
@@ -59,6 +74,12 @@ public class ClasePrincipalConcurrente {
             }
         }
 
+        // fin cronómetro 2
+        long finCronom = System.currentTimeMillis(); // se para el cronometro
+        d2.setTime(finCronom);
+        System.out.println(
+                "Cálculo finalizado a las " + df2.format(d2) + " tras " + (finCronom - inicCronom2) + " milisegundos");
+
         int iterRes = 0;
         for (int i = 0; i < matrices.length; i++) {
             for (int k = 0; k < matrices[i].matResParcial.length && iterRes + k < matRes.length; k++) {
@@ -66,6 +87,11 @@ public class ClasePrincipalConcurrente {
             }
             iterRes += matrices[i].matResParcial.length;
         }
+
+        // fin cronómetro 1
+        System.out.println(
+                "Proceso finalizado a las " + df1.format(d1) + " tras " + (finCronom - inicCronom1) + " milisegundos");
+
         scan.close();
         System.out.println("Fin.");
     }
--- PCTR/RELACION3/EJ2/matVector.java
+++ PCTR/RELACION3/EJ2/matVector.java
@@ -1,3 +1,5 @@
+import java.text.*;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,7 +8,8 @@ import java.util.Scanner;
  * matriz [n], ambas de valores enteros.
  * 
  * Está probado con un tamaño máximo de n = 30000 añadiendo necesariamente los
- * flags -Xmx4G -ea
+ * flags -Xmx4G y con un tamaño máximo de n = 50000 con el flag -Xmx10G. Por sí
+ * solo recomendamos no superar n = 10000.
  * 
  * @author Javier López Sierra
  */
@@ -15,10 +18,18 @@ public class matVector {
     static int[] matrizB;
 
     public static void main(String[] args) {
+
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
         System.out.println("Introduzca tamaño de las matrices:");
         int n = scan.nextInt();
+
+        // cronómetro 1 (proceso completo)
+        Date d1 = new Date();
+        DateFormat df1 = new SimpleDateFormat("HH:mm:ss:SSS");
+        long inicCronom1 = System.currentTimeMillis(); // se prepara el cronometro
+        d1.setTime(inicCronom1); // se activa el cronometro
+
         matrizCuadrada = new int[n][n];
         matrizB = new int[n];
         int[] matRes = new int[n]; // matriz resultado
@@ -30,11 +41,27 @@ public class matVector {
             matRes[i] = 0;
         }
 
+        // cronómetro 2 (cálculo)
+        Date d2 = new Date();
+        DateFormat df2 = new SimpleDateFormat("HH:mm:ss:SSS");
+        long inicCronom2 = System.currentTimeMillis(); // se prepara el cronometro
+        d2.setTime(inicCronom2); // se activa el cronometro
+
         for (int i = 0; i < n; i++) { // se hace el producto secuencial
             for (int j = 0; j < n; j++) {
                 matRes[i] += matrizCuadrada[i][j] * matrizB[j];
             }
         }
+
+        // fin cronómetro 2
+        long finCronom = System.currentTimeMillis(); // se para el cronometro
+        d2.setTime(finCronom);
+        System.out.println(
+                "Cálculo finalizado a las " + df2.format(d2) + " tras " + (finCronom - inicCronom2) + " milisegundos");
+
+        // fin cronómetro 1
+        System.out.println(
+                "Proceso finalizado a las " + df1.format(d1) + " tras " + (finCronom - inicCronom1) + " milisegundos");
 
         System.out.println("Fin.");
         scan.close();
