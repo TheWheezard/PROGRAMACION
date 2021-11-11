ef186ee16aa103125baa35a1f8f38d330a8c2938
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-11-11 15:55:15 +0100

    Update algEisenbergMcGuire.java

--- PCTR/RELACION4/algEisenbergMcGuire.java
+++ PCTR/RELACION4/algEisenbergMcGuire.java
@@ -1,4 +1,19 @@
 /** */
 public class algEisenbergMcGuire {
-    
+    public int n = 0;
+    private static volatile int numVueltas = 10000;
+    public enum estado {IDLE, WAIT, ACTIVE};
+    public estado[] flags;
+    int turno;
+    int tipoHilo;
+
+    public algEisenbergMcGuire(int tipoHilo) {
+        this.tipoHilo = tipoHilo;
+    }
+
+    public void run() {
+        for (int i = 0; i < numVueltas; i++) {
+            
+        }
+    }
 }
