6ddf1af296e0e9d236ba34b9e884da4b96310fb2
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-11-17 17:30:14 +0100

    Ejercicios
    Ejercicio 3 incompleto
    Resto por mirar

---
+++ PCTR/RELACION5/Ejercicio3/matVector.java
@@ -0,0 +1,67 @@
+import java.text.*;
+import java.util.Date;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * La clase matVector realiza el producto de una matriz cuadrada [n][n] por otra
+ * matriz [n], ambas de valores enteros.
+ * 
+ * Está probado con un tamaño máximo de n = 30000 añadiendo necesariamente el
+ * flag -Xmx4G y con un tamaño máximo de n = 50000 con el flag -Xmx10G. Por sí
+ * solo recomendamos no superar n = 10000.
+ * 
+ * @author Javier López Sierra
+ */
+public class matVector {
+    static int[][] matrizCuadrada;
+    static int[] matrizB;
+
+    public static void main(String[] args) {
+
+        Random random = new Random();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduzca tamaño de las matrices:");
+        int n = scan.nextInt();
+
+        // cronómetro 1 (proceso completo)
+        Date d1 = new Date();
+        DateFormat df1 = new SimpleDateFormat("HH:mm:ss:SSS");
+        long inicCronom1 = System.currentTimeMillis(); // se prepara el cronometro
+        d1.setTime(inicCronom1); // se activa el cronometro
+
+        matrizCuadrada = new int[n][n];
+        matrizB = new int[n];
+        int[] matRes = new int[n]; // matriz resultado
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrizCuadrada[i][j] = random.nextInt();
+            }
+            matrizB[i] = random.nextInt();
+            matRes[i] = 0;
+        }
+
+        // cronómetro 2 (cálculo)
+        Date d2 = new Date();
+        DateFormat df2 = new SimpleDateFormat("HH:mm:ss:SSS");
+        long inicCronom2 = System.currentTimeMillis(); // se prepara el cronometro
+        d2.setTime(inicCronom2); // se activa el cronometro
+
+        for (int i = 0; i < n; i++) { // se hace el producto secuencial
+            for (int j = 0; j < n; j++) {
+                matRes[i] += matrizCuadrada[i][j] * matrizB[j];
+            }
+        }
+
+        // fin cronómetro 2
+        long finCronom = System.currentTimeMillis(); // se para el cronometro
+        d2.setTime(finCronom);
+        System.out.println("Cálculo finalizado a las " + df2.format(d2) + " tras " + (finCronom - inicCronom2) + " milisegundos");
+
+        // fin cronómetro 1
+        System.out.println("Proceso finalizado a las " + df1.format(d1) + " tras " + (finCronom - inicCronom1) + " milisegundos");
+
+        System.out.println("Fin.");
+        scan.close();
+    }
+}
---
+++ PCTR/RELACION5/Ejercicio3/matVectorConcurrente.java
@@ -0,0 +1,59 @@
+/**
+ * La clase matVectorConcurrente recibe un conjunto de filas de una matriz
+ * cuadrada[n][n] y la posición de la fila inicial y realiza el producto con
+ * otra matriz[n], guardando el resultado parcial en un array. Ambas matrices
+ * están rellenas de valores enteros aleatorios.
+ * 
+ * @author Javier López Sierra
+ * @see Java.lang.Runnable
+ */
+public class matVectorConcurrente implements Runnable {
+    int idHilo;
+    int filas; // filas de la matriz cuadrada
+    int inicio; // fila inicial de la matriz cuadrada
+    static int n = 500;
+    int[] matResParcial;
+
+    /**
+     * Constructor para la clase matVectorConcurrente.
+     * 
+     * @param idHilo Parámetro que identifica el objeto.
+     * @param filas  Parámetro que indica el número de filas de la matriz cuadrada
+     *               asignados para su cálculo.
+     * @param inicio Parámetro que indica la fila de la matriz cuadrada desde la que
+     *               se empieza el cálculo.
+     */
+    public matVectorConcurrente(int idHilo, int filas, int inicio) {
+        this.idHilo = idHilo;
+        this.filas = filas;
+        this.inicio = inicio;
+        matResParcial = new int[filas]; // las n * x/total_partes filas que le tocan
+
+    }
+
+    /**
+     * Método modificador que realiza el producto escalar de un grupo de filas de
+     * una matriz cuadrada[n][n] con una matriz[n] de la clase
+     * ClasePrincipalVector.
+     * 
+     * @see ClasePrincipalVector
+     */
+    public void calcular() {
+        for (int i = 0; i < matResParcial.length && inicio < n; i++) {
+            int res = 0;
+            for (int j = 0; j < ClasePrincipalVector.matrizCuadradaB.length; j++) {//MODIFICAR
+                res = res + ClasePrincipalVector.matrizCuadradaA[inicio + i][j] * ClasePrincipalVector.matrizCuadradaB[i][j];
+            }
+            matResParcial[i] = res;
+        }
+    }
+
+    /**
+     * Sobrecarga del método run() de la interfaz Runnable.
+     * 
+     * @see Java.lang.Runnable.run
+     */
+    public void run() {
+        this.calcular();
+    }
+}
