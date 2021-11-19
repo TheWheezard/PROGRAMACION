9efecaa340c27fa12824ac0a0800f4e800f04693
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-11-19 15:00:35 +0100

    P5 Update

--- PCTR/RELACION5/primosParalelos.java
+++ PCTR/RELACION5/primosParalelos.java
@@ -1,3 +1,4 @@
+
 /*primosParalelos.java
  *@author A.T.
 */
@@ -7,37 +8,34 @@ import java.util.*;
 public class primosParalelos {
 
   public static void main(String[] args) throws Exception {
-    long nPuntos     = Integer.parseInt(args[0]);
-    int  nTareas     = Runtime.getRuntime().availableProcessors();
-    long tVentana    = nPuntos/nTareas;
+    long nPuntos = Integer.parseInt(args[0]);
+    int nTareas = Runtime.getRuntime().availableProcessors();
+    //int nTareas = 32;
+    long tVentana = nPuntos / nTareas;
     long primosTotal = 0;
-    long linf        = 0;
-    long lsup        = tVentana;
-    
-    ArrayList<Future<Long>> contParciales =
-      new ArrayList<Future<Long>>();
-    long inicTiempo = System.nanoTime();  
-    ThreadPoolExecutor ept = new ThreadPoolExecutor(
-      nTareas,
-      nTareas,
-      0L,
-      TimeUnit.MILLISECONDS,
-      new LinkedBlockingQueue<Runnable>());
-    for(int i=0; i<nTareas; i++){
-      contParciales.add(ept.submit(
-      	 new tareaPrimos(linf, lsup)));
-      linf=lsup+1;
-      lsup+=tVentana;
-    }  
-    for(Future<Long> iterador:contParciales)
-      try{
-      	  primosTotal +=  iterador.get(); 
-      }catch (CancellationException e){}
-       catch (ExecutionException e){}
-       catch (InterruptedException e){}     
-    long tiempoTotal = (System.nanoTime()-inicTiempo)/(long)1.0e9;   
+    long linf = 0;
+    long lsup = tVentana;
+
+    ArrayList<Future<Long>> contParciales = new ArrayList<Future<Long>>();
+    long inicTiempo = System.nanoTime();
+    ThreadPoolExecutor ept = new ThreadPoolExecutor(nTareas, nTareas, 0L, TimeUnit.MILLISECONDS,
+        new LinkedBlockingQueue<Runnable>());
+    for (int i = 0; i < nTareas; i++) {
+      contParciales.add(ept.submit(new tareaPrimos(linf, lsup)));
+      linf = lsup + 1;
+      lsup += tVentana;
+    }
+    for (Future<Long> iterador : contParciales)
+      try {
+        primosTotal += iterador.get();
+      } catch (CancellationException e) {
+      } catch (ExecutionException e) {
+      } catch (InterruptedException e) {
+      }
+    long tiempoTotal = (System.nanoTime() - inicTiempo) / (long) 1.0e9;
     ept.shutdown();
-    System.out.println("Primos hallados: "+primosTotal);
-    System.out.println("Calculo finalizado en "+tiempoTotal+" segundos");
- }   
+    System.out.println("Primos hallados: " + primosTotal);
+    System.out.println("nTareas: " + nTareas);
+    System.out.println("Calculo finalizado en " + tiempoTotal + " segundos");
+  }
 }
--- PCTR/RELACION5/tareaPrimos.java
+++ PCTR/RELACION5/tareaPrimos.java
@@ -1,25 +1,29 @@
 import java.util.concurrent.Callable;
-public class tareaPrimos implements Callable{
-   
+
+public class tareaPrimos implements Callable {
+
   private final long linf;
   private final long lsup;
   private Long total = new Long(0);
-   	
-  public tareaPrimos(long linf, long lsup){
+
+  public tareaPrimos(long linf, long lsup) {
     this.linf = linf;
     this.lsup = lsup;
   }
-  
-  public boolean esPrimo(long n){
-    if(n<=1) return(false);
-    for(long i=2; i<=Math.sqrt(n); i++)
-      if(n%i == 0) return(false);
-    return(true);
-  }	
-		
-  public Long call(){   
-    for(long i=linf; i<=lsup;i++)
-      if(esPrimo(i)) total++;
-  return(total);
+
+  public boolean esPrimo(long n) {
+    if (n <= 1)
+      return (false);
+    for (long i = 2; i <= Math.sqrt(n); i++)
+      if (n % i == 0)
+        return (false);
+    return (true);
+  }
+
+  public Long call() {
+    for (long i = linf; i <= lsup; i++)
+      if (esPrimo(i))
+        total++;
+    return (total);
   }
 }
