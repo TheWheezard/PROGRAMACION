bc700366d56309bdeb0ffc8cd9ca749115b49c36
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-12-02 20:45:15 +0100

    Create CrawlerDownloader.java

---
+++ RI/CRAWLER_RI/CrawlerDownloader.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * CrawlerDownloader
+ */
+public class CrawlerDownloader {
+    public static void main(String[] args) throws IOException {
+        URL web = new URL("https://en.wikipedia.org/wiki/Natural_language");
+        URLConnection conectar = web.openConnection();
+        BufferedReader br = new BufferedReader(new InputStreamReader(conectar.getInputStream()));
+        String cadURL = new String();
+        Pattern pat = Pattern.compile(".*href=\"/wiki/(.*[^:]+.*)\"");
+        Pattern pat2 = Pattern.compile(".*https://en.wikipedia.org/wiki/(.*[^:]+.*)");//
+        Matcher mat;
+        Matcher mat2;
+        while ((cadURL = br.readLine()) != null) {
+            //System.out.println(cadURL);
+            mat = pat.matcher(cadURL);
+            if (mat.matches()) {
+                System.out.println(cadURL);
+            }
+        }
+    }
+}
