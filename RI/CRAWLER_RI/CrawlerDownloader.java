91004a34fe364064c87c545b012defeff806c27b
Javi-LS <javi.lopezsierra@alum.uca.es>
2021-12-09 19:32:14 +0100

    Update CrawlerDownloader.java
    arreglar escritura fichero

--- RI/CRAWLER_RI/CrawlerDownloader.java
+++ RI/CRAWLER_RI/CrawlerDownloader.java
@@ -8,21 +8,39 @@ import java.util.regex.Pattern;
  * CrawlerDownloader
  */
 public class CrawlerDownloader {
-    public static void main(String[] args) throws IOException {
-        URL web = new URL("https://en.wikipedia.org/wiki/Natural_language");
+    public static void main(String[] args) {
+        String web = new String("https://en.wikipedia.org/wiki/Natural_language");
+        try {
+            getURLs(web);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void getURLs(String dirWeb) throws IOException {
+        URL web = new URL(dirWeb);
         URLConnection conectar = web.openConnection();
         BufferedReader br = new BufferedReader(new InputStreamReader(conectar.getInputStream()));
         String cadURL = new String();
-        Pattern pat = Pattern.compile(".*(href=\"/wiki/\\w*(?![:])\\w*\").*");
-        Pattern pat2 = Pattern.compile(".*https://en.wikipedia.org/wiki/(.*[^:]+.*)");//
+        Pattern pat = Pattern.compile(".*href=\"(/wiki/\\w*(?![:])\\w*)\".*");
+        //Pattern pat2 = Pattern.compile(".*https://en.wikipedia.org/wiki/(.*[^:]+.*)");//
         Matcher mat;
-        Matcher mat2;
+        //Matcher mat2;
+        File fich = new File("direcciones.txt");
+        FileWriter writer = new FileWriter(fich);
+        BufferedWriter bfwriter = new BufferedWriter(writer);
+        System.out.println("Extrayendo direcciones...");
         while ((cadURL = br.readLine()) != null) {
             //System.out.println(cadURL);
             mat = pat.matcher(cadURL);
+            String cad = "";
             if (mat.matches()) {
-                System.out.println(mat.group(1));
+                cad = cad + mat.group(1);
+                System.out.println(cad);
+                //bfwriter.append(cad);
+                //bfwriter.newLine();
             }
+            bfwriter.write(cad);
         }
     }
 }
