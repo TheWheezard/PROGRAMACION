988d8b6ac30d2d28e1b8ff04ac3e75230bd56cc1
Javi Lopez <javi.lopezsierra@alum.uca.es>
2022-11-04 16:32:16 +0100

    Update algDekker.java
    Introducido el algoritmo de control de exclusión mutua en una función aparte
    run() llamará a esa función aparte según nVueltas

--- PCTR/RELACION4/algDekker.java
+++ PCTR/RELACION4/algDekker.java
@@ -1,5 +1,5 @@
 /**
- * Clase que emplea el algoritmo de Dekker en su forma final, en el que se
+ * Clase que emplea el <b>algoritmo de Dekker</b> en su forma final, en el que se
  * elimina el problema de espera infinita (livelock) y se evita el problema de
  * postergación indefinida mediante una variable que ayuda a desempatar al
  * competir por el turno
@@ -25,16 +25,12 @@ public class algDekker extends Thread {
     }
 
     /**
-     * Sobrecarga del método run() de la clase Thread que emplea la aproximación
-     * final del algoritmo de Dekker para comprobar si el hilo tiene el turno de
-     * paso para modificar una variable n
-     * 
-     * @see Thread#run()
+     * Función que emplea la aproximación final del algoritmo de Dekker para
+     * comprobar si el hilo tiene el turno de paso para modificar una variable n.
      */
-    public void run() {
+    public void exclusionMutua(){
         switch (tipoHilo) {
-        case 1:
-            for (int i = 0; i < nVueltas; i++) {
+            case 1:
                 C1 = true; // Solicita acceso a la SC
                 while (C2 == true) { // Si ya fue solicitado por el otro hilo
                     if (turno == 2) { // y es turno del otro hilo
@@ -48,10 +44,9 @@ public class algDekker extends Thread {
 
                 turno = 2; // Fin de la SC, cede el turno
                 C1 = false; // Señala que ha finalizado su uso del recurso
-            }
+
             break;
-        case 2:
-            for (int i = 0; i < nVueltas; i++) {
+            case 2:
                 C2 = true; // Solicita acceso a la SC
                 while (C1 == true) { // Si ya fue solicitado por el otro hilo
                     if (turno == 1) { // y es turno del otro hilo
@@ -65,8 +60,23 @@ public class algDekker extends Thread {
 
                 turno = 1; // Fin de la SC, cede el turno
                 C2 = false; // Señala que ha finalizado su uso del recurso
-            }
+
             break;
+        }
+    }
+
+    /**
+     * Sobrecarga del método {@code run()} de la clase <b>Thread</b> que, mediante
+     * un bucle, realiza varios accesos a una variable compartida mediante la
+     * función {@code exclusionMutua()}.
+     * 
+     * @see Thread#run()
+     * @see algDekker#exclusionMutua()
+     */
+    @Override
+    public void run() {
+        for (int i = 0; i < nVueltas; ++i) {
+            exclusionMutua();
         }
     }
 
