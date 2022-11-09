19e7f0cdef299bcaae31c88caabdfbba0a9764aa
Javi Lopez <javi.lopezsierra@alum.uca.es>
2022-11-09 16:32:47 +0100

    Actualizar ej Red
    Código reestructurado ligeramente
    Faltan comentarios, pero no hará falta entrega

--- PCTR/RELACION5/volcadoRedSecuencial.java
+++ PCTR/RELACION5/volcadoRedSecuencial.java
@@ -1,38 +1,42 @@
-import java.util.*;
 import java.io.*;
 import java.net.*;
 
-public class volcadoRedSecuencial{
+public class volcadoRedSecuencial {
 
-  public static void descargar(String dir, int i){
-      try{
-         URL url = new URL(dir);
-         BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-          FileWriter          f = new FileWriter(i+".html");
-         BufferedWriter writer = new BufferedWriter(f);
-         String line;
-         while ((line = reader.readLine()) != null){writer.write(line);}
-         f.close();
-         System.out.println(dir+" descargada...");
-      }catch(IOException e){}
-   } 
-  
-  public static void main(String[] args){
-
-    int cont=0;
-    long iniTiempo = System.nanoTime();	  
+  public static void descargar(String dir, int i) {
     try {
-    	 String linea=" ";      
-    	 RandomAccessFile direcciones = new RandomAccessFile("direccionesRed.txt","r");
-         while(linea!=null){
-           linea =(String)direcciones.readLine();
-           if(linea!=null)descargar(linea, cont);
-           cont++;
-         }
-         direcciones.close();
-        }catch (FileNotFoundException e) {}
-         catch (IOException e) {}
+      URL url = new URL(dir);
+      BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+      FileWriter f = new FileWriter(i + ".html");
+      BufferedWriter writer = new BufferedWriter(f);
+      String line;
+      while ((line = reader.readLine()) != null) {
+        writer.write(line);
+      }
+      f.close();
+      System.out.println(dir + " descargada...");
+    } catch (IOException e){}
+  }
+
+  public static void main(String[] args) {
+
+    int cont = 0;
+    long iniTiempo = System.nanoTime();
+    try {
+      String linea = " ";
+      RandomAccessFile direcciones = new RandomAccessFile("direccionesRed.txt", "r");
+      while (linea != null) {
+        linea = (String) direcciones.readLine();
+        if (linea != null)
+          descargar(linea, cont);
+        cont++;
+      }
+      direcciones.close();
+    } 
+    catch (FileNotFoundException e){} 
+    catch (IOException e){}
     long finTiempo = System.nanoTime();
-    System.out.println("Tiempo Total (segundos): "+(finTiempo-iniTiempo)/1.0e9);
-  }  
+    System.out.println("Tiempo Total (segundos): " + (finTiempo - iniTiempo) / 1.0e9);
+  }
 }
+//TODO comentarios Javadoc
