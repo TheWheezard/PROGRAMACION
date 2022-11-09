5b2c03a184947ac2a7968a5ffe4499f0b30e51cb
Javi Lopez <javi.lopezsierra@alum.uca.es>
2022-11-09 16:29:54 +0100

    Update tareaPrimos.java
    Callable ahora es Callable<Long> (solución de warnings)
    Faltan comentarios

--- PCTR/RELACION5/tareaPrimos.java
+++ PCTR/RELACION5/tareaPrimos.java
@@ -1,6 +1,6 @@
 import java.util.concurrent.Callable;
 
-public class tareaPrimos implements Callable {
+public class tareaPrimos implements Callable<Long> {
 
   private final long linf;
   private final long lsup;
@@ -27,3 +27,4 @@ public class tareaPrimos implements Callable {
     return (total);
   }
 }
+//TODO comentarios Javadoc
