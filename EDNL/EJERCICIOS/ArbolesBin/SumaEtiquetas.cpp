bc8de62b46533d8f8d19cf772844fe07f259dfb7
Javi-LS <javi.lopezsierra@alum.uca.es>
2023-06-20 00:28:23 +0200

    Create SumaEtiquetas.cpp

---
+++ EDNL/EJERCICIOS/ArbolesBin/SumaEtiquetas.cpp
@@ -0,0 +1,9 @@
+#include "..\..\Implementaciones\Abin.hpp"
+
+/**
+ * @brief Implemente una función llamada sumaEtiquetas que dados dos árboles
+ * binarios de tipo entero, devuelva la suma total de los nodos de las ramas
+ * (de raíz a hoja) de ambos árboles si se cumple que las mismas ramas de
+ * cada árbol suman igual. Se devolverá 0 en caso contrario.
+ */
+int sumaEtiquetas(Abin<int>& a, Abin<int>& b) {}
