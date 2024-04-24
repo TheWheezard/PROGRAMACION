69a0de4a2885f0fac5f1b28c43c220c3b9ab8a2c
Javi-LS <javi.lopezsierra@alum.uca.es>
2024-04-24 21:45:08 +0200

    Update comprobarAVL.cpp

--- EDNL/EJERCICIOS/ABB/comprobarAVL.cpp
+++ EDNL/EJERCICIOS/ABB/comprobarAVL.cpp
@@ -1,45 +1,66 @@
 #include "..\..\Implementaciones\Abin.hpp"
 #include <vector>
 
-/* Dado un árbol binario, comprobar si este es un AVL, árbol binario de búsqueda equilibrado*/
+/* Dado un árbol binario, comprobar si este es un AVL, árbol binario de búsqueda equilibrado */
 
 // Recibe un árbol binario genérico (ref)
 // Devuelve true si cumple la especificación de un AVL, false en caso contrario
 template<typename T>
 bool esAVL(Abin<T>& A) {
-    std::vector<T> vec = new std::vector<T>;
-    rellenarVector(A, A.raiz(), vec);
-    return elementosOrdenados(vec) && estaEquilibrado(A, A.raiz());
+    // std::vector<T> vec = new std::vector<T>;
+    // rellenarVector(A, A.raiz(), vec);
+    // return elementosOrdenados(vec) && estaEquilibrado(A, A.raiz());
+    return comparaElementos(A, A.raiz()) && estaEquilibrado(A, A.raiz());
 }
 
-// Recibe un árbol binario (ref), un nodo que pertenece al árbol y un vector (ref)
-// Devuelve (ref) el vector relleno con los elementos del árbol, en inorden
+// @param A un árbol binario (referencia) 
+// @param n un nodo que pertenece al árbol
+// @param vec un vector (referencia)
+// @return (ref) el vector relleno con los elementos del árbol, en inorden
+// @deprecated ya no se rellena el vector y se compara directamente sobre los elementos del árbol
 template<typename T>
 void rellenarVector(Abin<T>& A, typename Abin<T>::nodo n, std::vector<T>& vec) {
-    if (n != Abin<T>::NODO_NULO) {
+    if (n != Abin<T>::NODO_NULO) { 
         rellenarVector(A, A.hijoIzqdo(n), vec);
         vec.push_back(A.elemento(n));
         rellenarVector(A, A.hijoDrcho(n), vec);
     }
 }
 
-// Recibe un vector (ref)
-// Devuelve true si todos los elementos están ordenados de menor a mayor, false en caso contrario
+// @param vec Un vector por referencia
+// @returns Devuelve true si todos los elementos están ordenados de menor a mayor, false en caso contrario
+// @deprecated Ya no se compara el vector y se compara directamente sobre los elementos del árbol
 template<typename T>
 bool elementosOrdenados(std::vector<T>& vec) {
     int i = 1;
     bool condicion = true;
     while (i < vec.size() && condicion) {
-        if (vec[i-1] > vec[i]) {
-            condicion = false;
-        }
+        condicion = vec[i-1] < vec[i];
         ++i;
     }
     return condicion;
 }
 
-// Recibe un árbol binario (ref) y un nodo perteneciente al árbol
-// Devuelve si ese nodo está equilibrado o no (factor de equilibrio eq: -1 <= eq <= 1)
+// @param A un árbol binario (referencia) 
+// @param n un nodo que pertenece al árbol
+// @return true si el árbol desde el nodo n cumple con la condición de orden
+template<typename T>
+bool comparaElementos(Abin<T>& A, typename Abin<T>::nodo n) {
+    if (n != Abin<T>::NODO_NULO) { 
+        bool checkIzdo = true;
+        bool checkDcho = true;
+        if (A.hijoIzqdo(n) != Abin<T>::NODO_NULO)
+            checkIzdo = A.elemento(A.hijoIzqdo(n)) < A.elemento(n) && comparaElementos(A, A.hijoIzqdo(n));
+        if (A.hijoDrcho(n) != Abin<T>::NODO_NULO)
+            checkDcho = A.elemento(A.hijoDrcho(n)) > A.elemento(n) && comparaElementos(A, A.hijoDrcho(n));
+        
+        return checkIzdo && checkDcho;
+    }
+}
+
+// @param A un árbol binario (ref)
+// @param n un nodo perteneciente al árbol
+// @return true si ese nodo está equilibrado (factor de equilibrio eq: -1 <= eq <= 1)
 template<typename T>
 bool estaEquilibrado(Abin<T>& A, typename Abin<T>::nodo n) {
     if (n != Abin<T>::NODO_NULO) {
@@ -47,31 +68,29 @@ bool estaEquilibrado(Abin<T>& A, typename Abin<T>::nodo n) {
         int alturaDcho = medirAltura(A, A.hijoDrcho(n));
         if (-1 <= (alturaIzdo - alturaDcho) && (alturaIzdo - alturaDcho) <= 1) {
             return true && estaEquilibrado(A, A.hijoIzqdo(n)) && estaEquilibrado(A, A.hijoDrcho(n));
-        }
-        else {
+        } else {
             return false;
         }
     }
     else return true;
 }
 
-// Recibe un árbol binario (ref) y un nodo perteneciente al árbol
-// Devuelve la altura máxima del nodo en el árbol
+// @param A un árbol binario (ref)
+// @param n un nodo perteneciente al árbol
+// @return la altura máxima del nodo en el árbol
 template<typename T>
 int medirAltura(Abin<T>& A, typename Abin<T>::nodo n) {
     if (n == Abin<T>::NODO_NULO) {
         return 0;
-    }
-    else {
+    } else {
         return 1 + maximo(medirAltura(A, A.hijoIzqdo(n)), medirAltura(A, A.hijoDrcho(n)));
     }
 }
 
-// Recibe dos enteros
-// Devuelve el máximo entre esos dos valores
+// @param a un entero
+// @param b un entero
+// @return el máximo entre esos dos valores
 int maximo(int a, int b) {
-    if (a >= b) {
-        return a;
-    }
+    if (a >= b) return a;
     else return b;
 }
