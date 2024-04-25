2bca4989955bf08b1d747bb6a487a70c6bf48b28
Javi-LS <javi.lopezsierra@alum.uca.es>
2024-04-25 21:35:36 +0200

    Update EliminarDescendientesPropios.cpp

--- EDNL/EJERCICIOS/ArbolesBin/EliminarDescendientesPropios.cpp
+++ EDNL/EJERCICIOS/ArbolesBin/EliminarDescendientesPropios.cpp
@@ -5,6 +5,42 @@ mayor o igual que el de sus ascendientes propios y menor o igual que el de sus d
 */
 #include "..\..\Implementaciones\Abin.hpp"
 
+/*INICIO*/
+//Transforma el Abin A que recibe de acuerdo al enunciado
+// T debe ser un tipo que permita ser ordenado (usando <,>,<=,>=)
+template<typename T>
+void transformarArbolBin(Abin<T>& A){
+    if (!A.arbolVacio()){
+        if (A.hijoIzqdo(A.raiz()) != Abin<T>::NODO_NULO)
+            recorrerAbin(A, A.hijoIzqdo(A.raiz()));
+        if (A.hijoDrcho(A.raiz()) != Abin<T>::NODO_NULO)
+            recorrerAbin(A, A.hijoDrcho(A.raiz()));
+    }
+}
+
+//Comprueba si el nodo cumple la condición del problema
+//y elimina los descendientes en caso afirmativo.
+//Se detiene cuando no hay más descendientes que recorrer
+template<typename T>
+void recorrerAbin(Abin<T>& A, typename Abin<T>::nodo n){
+    bool condicion = comparaAncestros(A, A.padre(n), A.elemento(n));
+    
+    if (A.hijoIzqdo(n) != Abin<T>::NODO_NULO) // si tiene hIzdo
+        condicion = condicion && comparaDescendientes(A, A.hijoIzqdo(n), A.elemento(n));
+    
+    if (A.hijoDrcho(n) != Abin<T>::NODO_NULO) // si tiene hDcho
+        condicion = condicion && comparaDescendientes(A, A.hijoDrcho(n), A.elemento(n));
+
+    // Si cumple la condición de los Ancestros y la condición de todos los Descendientes, hacemos poda:
+    if (condicion) podaAbin(A, n); 
+
+    // descendemos en el árbol
+    if (A.hijoIzqdo(n) != Abin<T>::NODO_NULO)
+        recorrerAbin(A, A.hijoIzqdo(n));
+    if (A.hijoDrcho(n) != Abin<T>::NODO_NULO)
+        recorrerAbin(A, A.hijoDrcho(n));
+}
+
 //Compara el valor de un nodo con todos sus ancestros propios recursivamente
 //Recibe el árbol, el ancestro del nodo que comparamos, y el elemento del nodo
 //Devuelve TRUE si TODOS los ascendientes propios son MENORES O IGUALES que el elemento del nodo
@@ -48,41 +84,5 @@ void podaAbin(Abin<T>& A, typename Abin<T>::nodo n){
     if (A.hijoDrcho(n) != Abin<T>::NODO_NULO){
         podaAbin(A, A.hijoDrcho(n));
         A.eliminarHijoDrcho(n);
-    }
-}
-
-//Comprueba si el nodo cumple la condición del problema
-//y elimina los descendientes en caso afirmativo.
-//Se detiene cuando no hay más descendientes que recorrer
-template<typename T>
-void recorrerAbin(Abin<T>& A, typename Abin<T>::nodo n){
-    bool condicion = comparaAncestros(A, A.padre(n), A.elemento(n));
-    
-    if (A.hijoIzqdo(n) != Abin<T>::NODO_NULO) // si tiene hIzdo
-        condicion = condicion && comparaDescendientes(A, A.hijoIzqdo(n), A.elemento(n));
-    
-    if (A.hijoDrcho(n) != Abin<T>::NODO_NULO) // si tiene hDcho
-        condicion = condicion && comparaDescendientes(A, A.hijoDrcho(n), A.elemento(n));
-
-    // Si cumple la condición de los Ancestros y la condición de todos los Descendientes, hacemos poda:
-    if (condicion) podaAbin(A, n); 
-
-    // descendemos en el árbol
-    if (A.hijoIzqdo(n) != Abin<T>::NODO_NULO)
-        recorrerAbin(A, A.hijoIzqdo(n));
-    if (A.hijoDrcho(n) != Abin<T>::NODO_NULO)
-        recorrerAbin(A, A.hijoDrcho(n));
-}
-
-/*INICIO*/
-//Transforma el Abin A que recibe de acuerdo al enunciado
-// T debe ser un tipo que permita ser ordenado (usando <,>,<=,>=)
-template<typename T>
-void transformarArbolBin(Abin<T>& A){
-    if (!A.arbolVacio()){
-        if (A.hijoIzqdo(A.raiz()) != Abin<T>::NODO_NULO)
-            recorrerAbin(A, A.hijoIzqdo(A.raiz()));
-        if (A.hijoDrcho(A.raiz()) != Abin<T>::NODO_NULO)
-            recorrerAbin(A, A.hijoDrcho(A.raiz()));
     }
 }
