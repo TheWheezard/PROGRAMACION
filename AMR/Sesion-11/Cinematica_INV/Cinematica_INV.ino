9caa7b9aa9b33f1b211149c8400633cfaddf1bbc
Javi-LS <javi.lopezsierra@alum.uca.es>
2024-05-15 21:45:03 +0200

    Update Cinematica_INV.ino

--- AMR/Sesion-11/Cinematica_INV/Cinematica_INV.ino
+++ AMR/Sesion-11/Cinematica_INV/Cinematica_INV.ino
@@ -4,31 +4,30 @@
 #include <InverseK.h>
 #include <Braccio.h>
 #include <Servo.h>
-// #include "Sensor.cpp"
 
+// Define los pines para el sensor CNY70
 #define S0 0   // D3
 #define S1 1   // D4
 #define S2 2   // D5
 #define S3 4   // D6
 #define OUT 10 // D2
-
-const int s0 = 3;
-const int s1 = 4;
-const int s2 = 5;
-const int s3 = 6;
-const int out = 2;
-
+// Define variables globales para el sensor CNY70
 int countRed = 0;
 int countGreen = 0;
 int countBlue = 0;
 int Pot = 0;
+
+// Define los pines para el sensor CNY70 (deprecated)
+// const int s0 = 3;
+// const int s1 = 4;
+// const int s2 = 5;
+// const int s3 = 6;
+// const int OUT = 2;
+
+
 // inicialización de entradas y salidas
-enum color { VACIO,
-             ROJO,
-             BLANCO,
-             NEGRO };
 
-
+// Define los servos
 Servo barrera;
 Servo base;
 Servo shoulder;
@@ -37,6 +36,12 @@ Servo wrist_rot;
 Servo wrist_ver;
 Servo gripper;
 
+enum color {
+  VACIO,
+  ROJO,
+  BLANCO,
+  NEGRO
+};
 
 
 void setup() {
@@ -45,12 +50,12 @@ void setup() {
   barrera.attach(8, 1000, 2000);  //Set the pin to attach the servo
   barrera.write(0);
 
-  // Init CNY
+  // Init CNY70
   pinMode(s0, OUTPUT);
   pinMode(s1, OUTPUT);
   pinMode(s2, OUTPUT);
   pinMode(s3, OUTPUT);
-  pinMode(out, INPUT);
+  pinMode(OUT, INPUT);
   digitalWrite(s0, HIGH);
   digitalWrite(s1, HIGH);
 
@@ -70,27 +75,11 @@ void setup() {
   // Attach the links to the inverse kinematic model
   InverseK.attach(base1, upperarm, forearm, hand);
 
-  float a0, a1, a2, a3;
-  float b0, b1, b2, b3;
-
   // InverseK.solve() return true if it could find a solution and false if not.
-
   // Calculates the angles without considering a specific approach angle
   // InverseK.solve(x, y, z, a0, a1, a2, a3)
-  if (InverseK.solve(0, 0, 505 - 75, a0, a1, a2, a3)) {
-    Serial.println("-0-");
-    Serial.print(a2b(a0));
-    Serial.print(',');
-    Serial.print(a2b(a1));
-    Serial.print(',');
-    Serial.print(a2b(a2));
-    Serial.print(',');
-    Serial.println(a2b(a3));
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-    Serial.println("INIT POSITION");
-  } else {
-    Serial.println("No solution found!");
-  }
+  Serial.println("INIT POSITION");
+  resetArm();
   /*
   // Calculates the angles considering a specific approach angle
   // InverseK.solve(x, y, z, a0, a1, a2, a3, phi)
@@ -119,135 +108,49 @@ void loop() {
   delay(5000);
   barrera.write(0);  // Bajar
 
-  // delay(100000);
-  // switch(c) {
-  //   case ROJO:
-  //     descargarRojo();
-  //     break;
-  //   case BLANCO:
-  //     descargarBlanco();
-  //     break;
-  //   case NEGRO:
-  //     descargarNegro();
-  //     break;
-
-  //   default;
-  // }
+  delay(100000);
+  switch(c) {
+    case ROJO:
+      descargarRojo();
+      break;
+    case BLANCO:
+      descargarBlanco();
+      break;
+    case NEGRO:
+      descargarNegro();
+      break;
+    default:
+    break;
+  }
   delay(8000);
 
   // RESET POSITION
-  float a0, a1, a2, a3;
-  if(InverseK.solve(0, 0, 505-75, a0, a1, a2, a3)) {
-    Serial.println("-0-");
-    Serial.print(a2b(a0)); Serial.print(',');
-    Serial.print(a2b(a1)); Serial.print(',');
-    Serial.print(a2b(a2)); Serial.print(',');
-    Serial.println(a2b(a3));
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-    Serial.println("RESET POSITION");
-  } else {
-    Serial.println("No solution found!");
-  }
-  while(true){}
+  resetArm();
+  while (true) {}
 }
 
 // @brief Mueve el cubo de la zona de recogida al sensor de color
 void comprobarCubo() {
-  float a0, a1, a2, a3;
-  float b0, b1, b2, b3;
-  InverseK.solve(0, 0, 505 - 75, a0, a1, a2, a3);
 
   // Orientar el brazo en la primera zona y sobre el cubo
-  if (InverseK.solve(0, -170, 100, b0, b1, b2, b3)) {
-    Serial.println("-1a-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 0, 10);
-  } else {
-    Serial.println("No solution found! -1a-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(0, -170, 100, 0, 10);
 
   // Agarrar el primer cubo
-  if (InverseK.solve(0, -170, -55, b0, b1, b2, b3)) {  // -75 ~= 0
-    Serial.println("-1b-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 0, 10);
-    delay(1000);
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 0, 45);
-  } else {
-    Serial.println("No solution found! -1b-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(0, -170, -55, 0, 10);
+  moveArm(0, -170, -55, 0, 45);
 
   // Elevar brazo con cubo
-  if (InverseK.solve(0, -170, 100, b0, b1, b2, b3)) {
-    Serial.println("-1a.-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 0, 45);
-  } else {
-    Serial.println("No solution found! -1a.-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(0, -170, 100, 0, 45);
 
   // Mover el cubo sobre el CNY
-  if (InverseK.solve(200, -78, 100, b0, b1, b2, b3)) {
-    Serial.println("-1c-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-  } else {
-    Serial.println("No solution found! -1c-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(200, -78, 100, 74, 45);
 
   // Orientar muñeca y colocar cubo
-  if (InverseK.solve(202, -76, 0, b0, b1, b2, b3)) {
-    Serial.println("-1d-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 76, 45);
-    delay(1000);
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 76, 10);
-  } else {
-    Serial.println("No solution found! -1d-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(202, -76, 0, 76, 45);
+  moveArm(202, -76, 0, 76, 10);
 
   // Soltar cubo
-  /* Hay que soltar y elevar*/
+  /* Hay que soltar y elevar? */
   // if(InverseK.solve(200, -61, 100, b0, b1, b2, b3)) {
   //   Serial.println("-1c-");
   //   Serial.print(a2b(b0)); Serial.print(',');
@@ -264,114 +167,27 @@ void comprobarCubo() {
 
 // @brief Mueve el cubo de la zona de color al vehículo en parada de carga
 void cargarCubo() {
-  float a0, a1, a2, a3;
-  float b0, b1, b2, b3;
-  InverseK.solve(0, 0, 505 - 75, a0, a1, a2, a3);
 
   // Orientar muñeca y recoger cubo
-  if (InverseK.solve(202, -76, 0, b0, b1, b2, b3)) {
-    Serial.println("-2a-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 10);
-    delay(1000);
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-  } else {
-    Serial.println("No solution found! -2a-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(202, -76, 0, 74, 10);
+  moveArm(202, -76, 0, 74, 45);
 
   // Elevar brazo con cubo
-  if (InverseK.solve(202, -76, 100, b0, b1, b2, b3)) {
-    Serial.println("-2b-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-  } else {
-    Serial.println("No solution found! -2b-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
+  moveArm(202, -76, 100, 74, 45);
 
   // Maniobra de giro hacia la zona de carga (retraer brazo)
-  if (InverseK.solve(130, -78, 100, b0, b1, b2, b3)) {
-    Serial.println("-2c-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-  } else {
-    Serial.println("No solution found! -2c-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-
+  moveArm(130, -78, 100, 74, 45);
+  
   // Maniobra de giro hacia la zona de carga (girar brazo)
-  if (InverseK.solve(0, 170, 100, b0, b1, b2, b3)) {
-    Serial.println("-2d-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-  } else {
-    Serial.println("No solution found! -2d-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(0, 170, 100, 74, 45);
 
   // Posicionar el brazo en la zona de carga
-  if (InverseK.solve(95, 180, 100, b0, b1, b2, b3)) {
-    Serial.println("-2e-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-  } else {
-    Serial.println("No solution found! -2e-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(95, 180, 100, 74, 45);
 
   // Bajar el brazo en la zona de carga
   // TODO: Ajustar la altura de la zona de carga y verificar posición del coche
-  if (InverseK.solve(95, 180, 0, b0, b1, b2, b3)) {
-    Serial.println("-2f-");
-    Serial.print(a2b(b0));
-    Serial.print(',');
-    Serial.print(a2b(b1));
-    Serial.print(',');
-    Serial.print(a2b(b2));
-    Serial.print(',');
-    Serial.println(a2b(b3));
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 45);
-    delay(1000);
-    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), 74, 10);
-  } else {
-    Serial.println("No solution found! -2f-");
-    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
-  }
-  delay(1000);
+  moveArm(95, 180, 0, 74, 45);
+  moveArm(95, 180, 0, 74, 10);
 }
 void descargarRojo() {}
 void descargarBlanco() {}
@@ -416,17 +232,21 @@ float a2b(float a) {
   return (a + HALF_PI) * 180 / PI;
 }
 
+//@brief Recibe los valores 0-255 de los sensores del CNY70
 void recibirColor() {
-    digitalWrite(s2, LOW);
-    digitalWrite(s3, LOW);
-    countRed = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-    digitalWrite(s3, HIGH);
-    countBlue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-    digitalWrite(s2, HIGH);
-    countGreen = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+  digitalWrite(S2, LOW);
+  digitalWrite(S3, LOW);
+  countRed = pulseIn(OUT, digitalRead(OUT) == HIGH ? LOW : HIGH);
+  digitalWrite(S3, HIGH);
+  countBlue = pulseIn(OUT, digitalRead(OUT) == HIGH ? LOW : HIGH);
+  digitalWrite(S2, HIGH);
+  countGreen = pulseIn(OUT, digitalRead(OUT) == HIGH ? LOW : HIGH);
 }
-// Programa principal
+
+// @brief Llama a recibirColor() e interpreta el valor recibido, devolviendo el color resultante
 color leerColor() {
+  color c = VACIO;
+  while (c == VACIO) {
     //Función de tomar color 
     recibirColor();
     //Lectura del potenciómetro
@@ -444,29 +264,69 @@ color leerColor() {
     // Recordatorio Valor del potenciómetro Pot = 185
     // Intervalo Rojo – Intervalo verde – Intervalo azul
     if (countRed > 30 && countRed < 45 && countGreen > 65 && countGreen < 80 && countBlue > 55 && countBlue < 75) {
-        Serial.println(" ‐ Color Rojo");
-        return ROJO;
+      Serial.println(" ‐ Color Rojo");
+      c = ROJO;
     }
-    // else if (countRed > 200 && countRed < 230 && countGreen > 185 && countGreen < 220 && countBlue > 80 && countBlue < 115) {
-    //     Serial.println(" ‐ Color Azul");
-    // }
-    // else if (countRed > 170 && countRed < 200 && countGreen > 80 && countGreen < 100 && countBlue > 35 && countBlue < 55) {
-    //     Serial.println(" ‐ Color 3");
-    // }
     else if (countRed > 50 && countRed < 60 && countGreen > 105 && countGreen < 120 && countBlue > 90 && countBlue < 110) {
-        Serial.println(" ‐ Color Blanco");
-        return BLANCO;
+      Serial.println(" ‐ Color Blanco");
+      c = BLANCO;
     }
-    // else if (countRed > 20 && countRed < 60 && countGreen > 20 && countGreen < 60 && countBlue > 180 && countBlue < 230) {
-    //     Serial.println(" ‐ Color Negro");
-    // }
     else if (countRed > 100 /*&& countGreen < 50 && countBlue > 170 && countBlue < 70*/) {
-        Serial.println(" ‐ Color Negro");
-        return NEGRO;
+      Serial.println(" ‐ Color Negro");
+      c = NEGRO;
     }
     else {
-        Serial.println("‐ VACIO");
-        return VACIO;
+      Serial.println("‐ VACIO");
     }
-    delay(300);
+    delay(1000);
+  }
+  return c;
+}
+
+// @brief Función genérica que mueve el brazo a la posición x,y,z mediante cinemática inversa.
+// @param x movimiento en eje X.
+// @param y movimiento en eje Y.
+// @param z movimiento en eje Z.
+// @param m rotación de muñeca.
+// @param p movimiento de pinza.
+// @note Si no encuentra solución, se pone en vertical.
+void moveArm(int x, int y, int z, int m, int p) {
+  float b0, b1, b2, b3;
+  if (InverseK.solve(x, y, z, b0, b1, b2, b3)) {
+    Serial.println("-move arm-");
+    Serial.print(a2b(b0));
+    Serial.print(',');
+    Serial.print(a2b(b1));
+    Serial.print(',');
+    Serial.print(a2b(b2));
+    Serial.print(',');
+    Serial.println(a2b(b3));
+    Braccio.ServoMovement(20, a2b(b0), a2b(b1), a2b(b2), a2b(b3), m, p);
+  }
+  else {
+    Serial.println("No solution found!");
+    resetArm();
+  }
+  delay(1000);
+}
+
+// @brief Función que pone el brazo casi en vertical, usando Cinemática Inversa.
+void resetArm() {
+  float a0, a1, a2, a3;
+  // InverseK.solve(x, y, z, a0, a1, a2, a3)
+  if (InverseK.solve(0, 0, 505 - 75, a0, a1, a2, a3)) {
+    Serial.println("-0-");
+    Serial.print(a2b(a0));
+    Serial.print(',');
+    Serial.print(a2b(a1));
+    Serial.print(',');
+    Serial.print(a2b(a2));
+    Serial.print(',');
+    Serial.println(a2b(a3));
+    Braccio.ServoMovement(20, a2b(a0), a2b(a1), a2b(a2), a2b(a3), 0, 20);
+  }
+  else {
+    Serial.println("No solution found!");
+  }
+  delay(1000);
 }
