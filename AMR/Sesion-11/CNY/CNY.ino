50544d9c4a13bdb83fea69d13be6c773122faa40
Javi Lopez <javi.lopezsierra@alum.uca.es>
2024-05-16 13:26:10 +0200

    Update CNY.ino

--- AMR/Sesion-11/CNY/CNY.ino
+++ AMR/Sesion-11/CNY/CNY.ino
@@ -14,12 +14,6 @@
 #define S3 4   // D6
 #define OUT 10 // D2
 
-const int s0 = 3;
-const int s1 = 4;
-const int s2 = 5;
-const int s3 = 6;
-const int out = 2;
-
 byte countRed = 0;
 byte countGreen = 0;
 byte countBlue = 0;
@@ -27,13 +21,13 @@ int Pot = 0;
 // inicialización de entradas y salidas
 void setup() {
     Serial.begin(9600);
-    pinMode(s0, OUTPUT);
-    pinMode(s1, OUTPUT);
-    pinMode(s2, OUTPUT);
-    pinMode(s3, OUTPUT);
-    pinMode(out, INPUT);
-    digitalWrite(s0, HIGH);
-    digitalWrite(s1, HIGH);
+    pinMode(S0, OUTPUT);
+    pinMode(S1, OUTPUT);
+    pinMode(S2, OUTPUT);
+    pinMode(S3, OUTPUT);
+    pinMode(OUT, INPUT);
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, HIGH);
 }
 // Programa principal
 void loop() {
@@ -53,8 +47,8 @@ void loop() {
 
     // Recordatorio Valor del potenciómetro Pot = 185
     // Intervalo Rojo – Intervalo verde – Intervalo azul
-    if (countRed > 30 && countRed < 45 && countGreen > 65 && countGreen < 80 && countBlue > 55 && countBlue < 75) {
-        Serial.println(" ‐ Color Rojo");
+    if (countRed > 9 && countRed < 15 && countGreen > 15 && countGreen < 20 && countBlue > 6 && countBlue < 15) {
+        Serial.println(" ‐ Color Blanco");
     }
     // else if (countRed > 200 && countRed < 230 && countGreen > 185 && countGreen < 220 && countBlue > 80 && countBlue < 115) {
     //     Serial.println(" ‐ Color Azul");
@@ -62,13 +56,13 @@ void loop() {
     // else if (countRed > 170 && countRed < 200 && countGreen > 80 && countGreen < 100 && countBlue > 35 && countBlue < 55) {
     //     Serial.println(" ‐ Color 3");
     // }
-    else if (countRed > 50 && countRed < 60 && countGreen > 105 && countGreen < 120 && countBlue > 90 && countBlue < 110) {
-        Serial.println(" ‐ Color Blanco");
+    else if (countRed > 13 && countRed < 20 && countGreen > 65 && countGreen < 75 && countBlue > 50 && countBlue < 60) {
+        Serial.println(" ‐ Color Rojo");
     }
     // else if (countRed > 20 && countRed < 60 && countGreen > 20 && countGreen < 60 && countBlue > 180 && countBlue < 230) {
     //     Serial.println(" ‐ Color Negro");
     // }
-    else if (countRed > 100 /*&& countGreen < 50 && countBlue > 170 && countBlue < 70*/) {
+    else if (countRed > 30 && countRed < 45 && countGreen > 140 && countGreen < 180 && countBlue > 120 && countBlue < 140) {
         Serial.println(" ‐ Color Negro");
     }
     else {
@@ -79,11 +73,11 @@ void loop() {
 
 //Función coger color 
 void getColor() {
-    digitalWrite(s2, LOW);
-    digitalWrite(s3, LOW);
-    countRed = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-    digitalWrite(s3, HIGH);
-    countBlue = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
-    digitalWrite(s2, HIGH);
-    countGreen = pulseIn(out, digitalRead(out) == HIGH ? LOW : HIGH);
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, LOW);
+    countRed = pulseIn(OUT, digitalRead(OUT) == HIGH ? LOW : HIGH);
+    digitalWrite(S3, HIGH);
+    countBlue = pulseIn(OUT, digitalRead(OUT) == HIGH ? LOW : HIGH);
+    digitalWrite(S2, HIGH);
+    countGreen = pulseIn(OUT, digitalRead(OUT) == HIGH ? LOW : HIGH);
 }
