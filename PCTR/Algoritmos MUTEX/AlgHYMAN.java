9bf67e6e6ed54cf8cb30e2c7ae4c4d6619d4a85d
Javi-LS <javi.lopezsierra@alum.uca.es>
2024-08-27 23:12:23 +0200

    Update AlgHYMAN.java

--- PCTR/Algoritmos MUTEX/AlgHYMAN.java
+++ PCTR/Algoritmos MUTEX/AlgHYMAN.java
@@ -1,22 +1,35 @@
 /**
  * Esta clase implementa el algoritmo Hyman para exclusión mutua.
  * El algoritmo es incorrecto y no proporciona una exclusión mutua adecuada.
- * Utiliza una variable compartida y un protocolo basado en turnos para controlar el acceso a la sección crítica.
- * Cada hilo entra en un bucle donde establece su propia bandera para indicar su deseo de entrar en la sección crítica.
- * Luego espera hasta que sea su turno de entrar, según el valor de la variable de turno compartida.
- * Sin embargo, este algoritmo no garantiza la exclusión mutua porque no evita que dos hilos entren en la sección crítica simultáneamente.
- * El algoritmo sufre la posibilidad de una condición de carrera donde ambos hilos establecen sus banderas y verifican la variable de turno simultáneamente, lo que lleva a una violación de la exclusión mutua.
- * Además, el algoritmo no maneja la inanición, ya que un hilo puede bloquearse indefinidamente esperando su turno.
- * Este algoritmo no debe usarse en entornos de producción donde se requiera exclusión mutua.
+ * Utiliza una variable compartida y un protocolo basado en turnos para
+ * controlar el acceso a la sección crítica.
+ * Cada hilo entra en un bucle donde establece su propia bandera para indicar su
+ * deseo de entrar en la sección crítica.
+ * Luego espera hasta que sea su turno de entrar, según el valor de la variable
+ * de turno compartida.
+ * Sin embargo, este algoritmo no garantiza la exclusión mutua porque no evita
+ * que dos hilos entren en la sección crítica simultáneamente.
+ * El algoritmo sufre la posibilidad de una condición de carrera donde ambos
+ * hilos establecen sus banderas y verifican la variable de turno
+ * simultáneamente, lo que lleva a una violación de la exclusión mutua.
+ * Además, el algoritmo no maneja la inanición, ya que un hilo puede bloquearse
+ * indefinidamente esperando su turno.
+ * Este algoritmo no debe usarse en entornos de producción donde se requiera
+ * exclusión mutua.
+ */
+/**
+ * Clase que implementa el algoritmo HYMAN para el acceso a una sección crítica por parte de dos hebras.
+ * El algoritmo utiliza una variable global compartida y un protocolo de turno para garantizar la exclusión mutua.
  */
 public class AlgHYMAN {
-    // Enumerated to represent the possible states of the global variable
+    // Enumerado para representar los posibles estados de la variable global
     private enum Estado {
         quiereentrar, restoproceso
     }
+
     private static volatile int Turno = 0;
 
-    // Shared global variable
+    // Variable global compartida
     private static volatile Estado C0 = Estado.restoproceso;
     private static volatile Estado C1 = Estado.restoproceso;
 
@@ -38,7 +51,8 @@ public class AlgHYMAN {
                     case 0:
                         C0 = Estado.quiereentrar;
                         while (Turno != 0) {
-                            while (C1 == Estado.quiereentrar);
+                            while (C1 == Estado.quiereentrar)
+                                ;
                             Turno = 0;
                         }
                         break;
@@ -46,7 +60,8 @@ public class AlgHYMAN {
                     case 1:
                         C1 = Estado.quiereentrar;
                         while (Turno != 1) {
-                            while (C0 == Estado.quiereentrar);
+                            while (C0 == Estado.quiereentrar)
+                                ;
                             Turno = 1;
                         }
                         break;
